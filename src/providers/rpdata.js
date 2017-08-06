436d5948adc2e90dc9ca062a639c049693ce8fc3
DaveRevay2 <daverevay@gmail.com>
2017-08-06 12:09:44 +1000

    misc

--- src/providers/rpdata.js
+++ src/providers/rpdata.js
@@ -1,48 +1,47 @@
 const { mapKeys, camelCase } = require('lodash')
 
 const nightmare = require('nightmare')({
-    show: true,
-    typeInterval: 70,
-});
+  show: true,
+  typeInterval: 70,
+})
 
-initialised = false;
+initialised = false
 
 const initialise = () => {
-
-    if (!initialised) {
-        nightmare
-        .goto('https://rpp.rpdata.com/rpp/login.html')
-        .type('#j_username', 'gerardcole')
-        .type('#j_password', 'gcp22222')
-        .click('.btn.btn--primary.floatLeft')
-        .wait('#searchAddressSimple input')
-        .then(() => {
-            initialised = true;
-        });
-    }
-};
+  if (!initialised) {
+    nightmare
+    .goto('https://rpp.rpdata.com/rpp/login.html')
+    .type('#j_username', 'gerardcole')
+    .type('#j_password', 'gcp22222')
+    .click('.btn.btn--primary.floatLeft')
+    .wait('#searchAddressSimple input')
+    .then(() => {
+      initialised = true
+    })
+  }
+}
 
 const search = ({ address }) => nightmare
-    .goto('https://rpp.rpdata.com/rpp/loadSummary.html')
-    .evaluate((address) => document.querySelector('#searchAddressSimple input').value = address, address)
-    .click('#addressLink')
-    .wait('.summaryListItem')
-    .evaluate(() => [ ...document.querySelectorAll('.summaryListItem') ].map(el => {
-      const result = {}
-      result.address = el.querySelector('h2 a').innerText
-      el.querySelectorAll('.summaryListItemContent li').forEach(liEl => {
-        const key = liEl.querySelector('label').innerText.replace(':', '')
-        const val = liEl.querySelector('span').innerText
-        result[key] = val
-      })
-      return result
-    }))
-     .then(properties => properties.map(property => mapKeys(property, (val, key) => camelCase(key))))
+  .goto('https://rpp.rpdata.com/rpp/loadSummary.html')
+  .evaluate((address) => document.querySelector('#searchAddressSimple input').value = address, address)
+  .click('#addressLink')
+  .wait('.summaryListItem')
+  .evaluate(() => [ ...document.querySelectorAll('.summaryListItem') ].map(el => {
+    const result = {}
+    result.address = el.querySelector('h2 a').innerText
+    el.querySelectorAll('.summaryListItemContent li').forEach(liEl => {
+      const key = liEl.querySelector('label').innerText.replace(':', '')
+      const val = liEl.querySelector('span').innerText
+      result[key] = val
+    })
+    return result
+  }))
+  .then(properties => properties.map(property => mapKeys(property, (val, key) => camelCase(key))))
 
-const isReady = () => initialised;
+const isReady = () => initialised
 
 module.exports = {
-    initialise,
-    isReady,
-    search,
-};
+  initialise,
+  isReady,
+  search,
+}
