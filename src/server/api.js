9cb27f2d1e92e2a28c9b38f1de4bac8cd2911222
Jackson Delahunt <jackson@stemn.com>
2017-08-07 13:56:52 +1000

    merge fix

--- src/server/api.js
+++ src/server/api.js
@@ -5,33 +5,33 @@ const { providersReady, rpData, whitePages } = require('../providers');
 
 const router = express.Router();
 
-router.get('/api/v1/restart', (req, res) => {
-    res.json('restarting...');
-    process.exit(0);
+app.get('/api/v1/restart', (req, res) => {
+  res.json('restarting...');
+  process.exit(0);
 });
 
-router.use((req, res, next) => {
+app.use((req, res, next) => {
+  // ensure all providers have initialised and ready to process before accepting requests
+  if (!providersReady()) {
+    res.status(SERVICE_UNAVAILABLE).json()
+  } else {
+    next()
+  }
+})
 
-    // ensure all providers have initialised and ready to process before accepting requests
-    if (!providersReady()) {
-        res.status(SERVICE_UNAVAILABLE).json();
-    } else {
-        next();
-    }
-});
+app.get('/api/v1/search', (req, res) => {
+  const { subPremise, streetNum, route, suburb, state, postcode, country } = req.query
+  const street = `${subPremise ? subPremise/ : ''} ${streetNum} ${route}` // Create the street string, i.e. 406/21 Enmore Rd
+  const address = `${street} ${suburb} ${state} ${postcode}` // Assemble the full address string the way RPdata likes it
+  rpData.search({ address })
+  .then(results => results.map(result => Object.assign(result, { suburb, postcode, state }))) // Suburb and postcode is added to the response
+  .then(results => res.json(results))
+})
 
-// Eg: http://localhost:3000/api/v1/search?address=406/21%20Enmore%20Road%20Newtown%20NSW%202042
-router.get('/api/v1/search', (req, res) => {
-
-    const { address } = req.query;
-
-    rpData.search({ address }).then((properties) => {
-        return properties;
-//        return Promise.map(properties, (property) => {
-//            return whitePages.search({ type: 'residential', name: property.fields['Owner Name'] });
-//        });
-    })
-    .then((results) => res.json(results));
-});
+app.get('/api/v1/people', (req, res) => {
+  const { name, postcode } = req.query
+  return whitePages.search(Object.assign(getNameDetails(name), { postcode }))
+  .then(results => res.json(results))
+})
 
 module.exports = router;
