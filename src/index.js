5c8991826b26915a7aeed80a13dc16434cc3f18e
DaveRevay2 <daverevay@gmail.com>
2017-08-08 21:04:42 +1000

    'version'

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const http = require('http');
 
 const server = require('./server');
+const packageJson = require('../package.json');
 const { initialise } = require('./providers');
 
 initialise();
@@ -8,3 +9,5 @@ initialise();
 const port = process.env.PORT || 3000;
 
 http.createServer(server).listen(port, () => console.log(`Server listening on port ${port}`));
+
+console.log(`Ownersearch api version: ${packageJson.version}`);
