a90b42330c8ca61e883d9c2f0679829b768a3c7f
DaveRevay2 <daverevay@gmail.com>
2017-08-22 20:12:27 +1000

    Rpdata can now recover after an outage

--- src/providers/rpdata.js
+++ src/providers/rpdata.js
@@ -1,24 +1,28 @@
 const { mapKeys, camelCase } = require('lodash')
 const qs = require('querystring')
+const Nightmare = require('nightmare')
 
-const nightmare = require('nightmare')({
+const nightmare = Nightmare({
   show: true,
   typeInterval: 70,
 })
 
-initialised = false
+
+let initialised = false
+
+const login = () => nightmare
+  .goto('https://rpp.rpdata.com/rpp/login.html')
+  .type('#j_username', 'gerardcole')
+  .type('#j_password', 'gcp22222')
+  .click('.btn.btn--primary.floatLeft')
+  .wait('#searchAddressSimple input')
+  .then(() => {
+    initialised = true
+  })
 
 const initialise = () => {
   if (!initialised) {
-    nightmare
-    .goto('https://rpp.rpdata.com/rpp/login.html')
-    .type('#j_username', 'gerardcole')
-    .type('#j_password', 'gcp22222')
-    .click('.btn.btn--primary.floatLeft')
-    .wait('#searchAddressSimple input')
-    .then(() => {
-      initialised = true
-    })
+    login()
   }
 }
 
@@ -42,14 +46,50 @@ const getResultData = () => [ ...document.querySelectorAll('.summaryListItem') ]
   return result
 })
 
-const search = ({ address }) => nightmare
-  .goto('https://rpp.rpdata.com/rpp/loadSummary.html')
-  .evaluate((address) => document.querySelector('#searchAddressSimple input').value = address, address)
-  .click('#addressLink')
-  .wait((addressEncoded) => location.search.includes(addressEncoded), qs.stringify(address))
-  .evaluate(getResultData)
-  .then(convertKeysToCamelCase)
-  .then(convertOwnerNameIntoArray)
+const splitRoute = (validSelector, errorSelector) => new Promise(resolve => {
+  const functionToRun = () => {
+    const validEl = document.querySelector(validSelector)
+    const errorEl = document.querySelector(errorSelector)
+    if (validEl) {
+      return 'valid'
+    } else if (errorEl) {
+      return 'error'
+    }
+  }
+
+  const interval = setInterval(() => {
+    const result = functionToRun()
+    if (result) {
+      clearInterval(interval)
+      resolve(result)
+    }
+  }, 50)
+})
+
+const search = ({ address }) => {
+  const startSearch = () => nightmare
+    .evaluate((address) => {
+      const inputEl = document.querySelector('#searchAddressSimple input')
+      if (inputEl) {
+        inputEl.value = address
+      }
+    }, address)
+    .click('#addressLink')
+    .wait((addressEncoded) => location.search.includes(addressEncoded), qs.stringify(address))
+    .evaluate(getResultData)
+    .then(convertKeysToCamelCase)
+    .then(convertOwnerNameIntoArray)
+
+  return nightmare
+    .goto('https://rpp.rpdata.com/rpp/loadSummary.html')
+    .evaluate(splitRoute, '#searchAddressSimple input', '.alert--error')
+    .then(data => {
+      if (data === 'valid') {
+        return startSearch()
+      }
+      return login().then(() => search({ address }))
+    })
+}
 
 const isReady = () => initialised
 
